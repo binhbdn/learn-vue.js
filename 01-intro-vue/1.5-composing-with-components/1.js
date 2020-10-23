a6672d04c120d8711f951b56b386f9217d213ff7
binhbdn <binh.buidn@gmail.com>
2020-10-23 22:14:47 +0700

    Vue.js - Composing with Components - App1

---
+++ 01-intro-vue/1.5-composing-with-components/1.js
@@ -0,0 +1,18 @@
+Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
