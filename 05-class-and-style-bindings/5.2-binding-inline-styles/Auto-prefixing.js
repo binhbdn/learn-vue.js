2fb78ef64b5476bb68cde02fb0ca38b2609aae1f
binhbdn <binh.buidn@gmail.com>
2020-10-29 22:37:37 +0700

    Auto-prefixing

---
+++ 05-class-and-style-bindings/5.2-binding-inline-styles/Auto-prefixing.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#app3',
+    data: {
+        baseStyles: {
+            color: 'blue',
+            'background-color': 'grey',
+            borderBottom: '5px solid red',
+            transform: 'rotate(-2deg)'
+        }
+    }
+})
