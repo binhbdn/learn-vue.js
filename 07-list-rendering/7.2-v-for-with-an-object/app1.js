2e937b3ae61c24baf912fb518ee842b8d5576620
binhbdn <binh.buidn@gmail.com>
2020-10-30 09:59:18 +0700

    v-for with an Object example No.1

---
+++ 07-list-rendering/7.2-v-for-with-an-object/app1.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: '#app1',
+    data: {
+        object: {
+            // tất nhiên chúng ta đều biết ông Bành Tổ không phải
+            // họ Bành tên Tổ, nhưng đây chỉ là một ví dụ…
+            'họ': 'Bành',
+            'tên': 'Tổ',
+            'tuổi': 800
+        }
+    }
+})
