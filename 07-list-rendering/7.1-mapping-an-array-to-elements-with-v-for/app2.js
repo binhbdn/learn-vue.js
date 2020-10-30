7db857a55f87b04d1ede292e82fdf8fca93f4659
binhbdn <binh.buidn@gmail.com>
2020-10-30 09:36:16 +0700

    Mapping an Array to Elements with v-for example 2

---
+++ 07-list-rendering/7.1-mapping-an-array-to-elements-with-v-for/app2.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#app2',
+    data: {
+        parentMessage: 'Parent',
+        items: [
+            { name: 'Cà phê' },
+            { name: 'Trà đặc' },
+            { name: 'Bò húc' }
+        ]
+    }
+})
