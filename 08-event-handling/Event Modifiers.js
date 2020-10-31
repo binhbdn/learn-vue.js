b08cd73708d3d8e1ddfb99c954d0425f936da66d
binhbdn <binh.buidn@gmail.com>
2020-10-31 21:53:44 +0700

    Event Modifiers

---
+++ 08-event-handling/Event Modifiers.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app4',
+    methods: {
+        doThis: function () {
+            alert('doThis')
+        },
+        doThat: function () {
+            alert('doThat')
+        },
+        onSubmit: function () {
+            alert('onSubmit')
+        },
+        donateKidney: function () {
+            alert('chỉ được kích hoạt tối đa một lần')
+        },
+        onScroll: function () {
+            alert('onScroll')
+        },
+    }
+})
