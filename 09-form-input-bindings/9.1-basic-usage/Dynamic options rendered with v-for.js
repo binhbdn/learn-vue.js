841d6754fbbff9732b022b4f0a1884101f9f0c24
binhbdn <binh.buidn@gmail.com>
2020-11-01 17:09:49 +0700

    Dynamic options rendered with v-for

---
+++ 09-form-input-bindings/9.1-basic-usage/Dynamic options rendered with v-for.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+  el: '#app7',
+  data: {
+    selected: '50k',
+    options: [
+      { text: 'Đọt bí xào tỏi', value: '50k' },
+      { text: 'Canh bông điên điển', value: '30k' },
+      { text: 'Lẩu nấm', value: '45k' }
+    ]
+  }
+})
