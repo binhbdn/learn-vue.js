f6fe6b8dec3504ae5d926a45ff4dd2f3040072c5
binhbdn <binh.buidn@gmail.com>
2021-08-27 08:19:36 +0700

    Content Distribution with Slots

---
+++ 10-components-basics/Content Distribution with Slots.js
@@ -0,0 +1,10 @@
+Vue.component('alert-box', {
+    template: `
+        <div class="demo-alert-box">
+            <strong>Error!</strong>
+            <slot></slot>
+        </div>
+    `
+  })
+
+new Vue({el: '#app9'})
