a9854022d225ae72c27713b7f60e77ef711050b3
binhbdn <binh.buidn@gmail.com>
2021-08-28 16:08:03 +0700

    Binding Native Events to Components

---
+++ 11-components-in-depth/11.3-custom-events/3 - Binding Native Events to Components.js
@@ -0,0 +1,44 @@
+Vue.component('base-input', {
+    inheritAttrs: false,
+    props: ['label', 'value'],
+    computed: {
+      inputListeners: function () {
+        var vm = this
+        // `Object.assign` merges objects together to form a new object
+        return Object.assign({},
+          // We add all the listeners from the parent
+          this.$listeners,
+          // Then we can add custom listeners or override the
+          // behavior of some listeners.
+          {
+            // This ensures that the component works with v-model
+            input: function (event) {
+              vm.$emit('input', event.target.value)
+            }
+          }
+        )
+      }
+    },
+    template: `
+      <label>
+        {{ label }}
+        <input
+          v-bind="$attrs"
+          v-bind:value="value"
+          v-on="inputListeners"
+        >
+      </label>
+    `
+})
+
+new Vue({
+    el: "#app3",
+    data: {
+        username: ''
+    },
+    methods: {
+        onBlur: function(event){
+            alert('BLUR event from ' + event.target.tagName)
+        }
+    }
+})
