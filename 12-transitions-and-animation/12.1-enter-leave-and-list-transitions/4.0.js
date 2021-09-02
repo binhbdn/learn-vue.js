44f9e3c2073a197f8752fad4fb62df0b6a6492a6
binhbdn <binh.buidn@gmail.com>
2021-09-02 21:55:39 +0700

    Transitioning Between Components

---
+++ 12-transitions-and-animation/12.1-enter-leave-and-list-transitions/4.0.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#transition-components-demo',
+  data: {
+    view: 'v-a'
+  },
+  components: {
+    'v-a': {
+      template: '<div>Component A</div>'
+    },
+    'v-b': {
+      template: '<div>Component B</div>'
+    }
+  }
+})
