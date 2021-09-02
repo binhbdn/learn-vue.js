534c5b1d580c1fe2e44d19e659e471d7b7304c1d
binhbdn <binh.buidn@gmail.com>
2021-09-02 18:15:30 +0700

    JavaScript Hooks

---
+++ 12-transitions-and-animation/12.1-enter-leave-and-list-transitions/1.7.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app17',
+    data: {
+      show: false
+    },
+    methods: {
+      beforeEnter: function (el) {
+        el.style.opacity = 0
+        el.style.transformOrigin = 'left'
+      },
+      enter: function (el, done) {
+        Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
+        Velocity(el, { fontSize: '1em' }, { complete: done })
+      },
+      leave: function (el, done) {
+        Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
+        Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+        Velocity(el, {
+          rotateZ: '45deg',
+          translateY: '30px',
+          translateX: '30px',
+          opacity: 0
+        }, { complete: done })
+      }
+    }
+})
