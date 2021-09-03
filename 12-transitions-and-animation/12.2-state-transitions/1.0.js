41a86f72cc6669974e9382b8ca7bcd737f25914e
binhbdn <binh.buidn@gmail.com>
2021-09-03 15:04:51 +0700

    Animating State with Watchers

---
+++ 12-transitions-and-animation/12.2-state-transitions/1.0.js
@@ -0,0 +1,67 @@
+new Vue({
+    el: '#animated-number-demo',
+    data: {
+      number: 0,
+      tweenedNumber: 0
+    },
+    computed: {
+      animatedNumber: function() {
+        return this.tweenedNumber.toFixed(0);
+      }
+    },
+    watch: {
+      number: function(newValue) {
+        gsap.to(this.$data, { duration: 0.5, tweenedNumber: newValue });
+      }
+    }
+})
+
+var Color = net.brehaut.Color
+
+new Vue({
+  el: '#example-7',
+  data: {
+    colorQuery: '',
+    color: {
+      red: 0,
+      green: 0,
+      blue: 0,
+      alpha: 1
+    },
+    tweenedColor: {}
+  },
+  created: function () {
+    this.tweenedColor = Object.assign({}, this.color)
+  },
+  watch: {
+    color: function () {
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+
+      new TWEEN.Tween(this.tweenedColor)
+        .to(this.color, 750)
+        .start()
+
+      animate()
+    }
+  },
+  computed: {
+    tweenedCSSColor: function () {
+      return new Color({
+        red: this.tweenedColor.red,
+        green: this.tweenedColor.green,
+        blue: this.tweenedColor.blue,
+        alpha: this.tweenedColor.alpha
+      }).toCSS()
+    }
+  },
+  methods: {
+    updateColor: function () {
+      this.color = new Color(this.colorQuery).toRGB()
+      this.colorQuery = ''
+    }
+  }
+})
