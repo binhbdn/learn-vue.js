ac8844c9ef3e0a9bc134a64b4ad297dcaaf095e8
binhbdn <binh.buidn@gmail.com>
2021-09-03 15:34:31 +0700

    Organizing Transitions into Components

---
+++ 12-transitions-and-animation/12.2-state-transitions/3.0.js
@@ -0,0 +1,60 @@
+// This complex tweening logic can now be reused between
+// any integers we may wish to animate in our application.
+// Components also offer a clean interface for configuring
+// more dynamic transitions and complex transition
+// strategies.
+Vue.component('animated-integer', {
+  template: '<span>{{ tweeningValue }}</span>',
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      tweeningValue: 0
+    }
+  },
+  watch: {
+    value: function (newValue, oldValue) {
+      this.tween(oldValue, newValue)
+    }
+  },
+  mounted: function () {
+    this.tween(0, this.value)
+  },
+  methods: {
+    tween: function (startValue, endValue) {
+      var vm = this
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+
+      new TWEEN.Tween({ tweeningValue: startValue })
+        .to({ tweeningValue: endValue }, 500)
+        .onUpdate(function () {
+          vm.tweeningValue = this.tweeningValue.toFixed(0)
+        })
+        .start()
+
+      animate()
+    }
+  }
+})
+
+// All complexity has now been removed from the main Vue instance!
+new Vue({
+  el: '#example-8',
+  data: {
+    firstNumber: 20,
+    secondNumber: 40
+  },
+  computed: {
+    result: function () {
+      return this.firstNumber + this.secondNumber
+    }
+  }
+})
